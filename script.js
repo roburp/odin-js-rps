f2d5dcd114d9dfafd62743f01d4da65d8d41925f
Robert Locsin <59687824+roburp@users.noreply.github.com>
2024-10-08 16:53:39 +0800

    Add UI and listeners to the RPS project

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let round = 0;
+let playerTally = 0;
+let computerTally = 0;
+
 function getComputerChoice() {
   switch (
     Math.floor(Math.random() * 3) // 0, 1, 2
@@ -12,8 +16,6 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  let choice = prompt("Rock, Paper or Scissors?").toLowerCase();
-
   switch (choice) {
     case "rock":
     case "paper":
@@ -24,38 +26,96 @@ function getHumanChoice() {
   }
 }
 
-function playRound() {
-  let humanChoice;
-  let computerChoice;
-  let round = 1;
-  let playerTally = 0;
-  let computerTally = 0;
+function playRound(choice) {
+  const humanChoice = choice;
+  const computerChoice = getComputerChoice();
+  round++;
 
-  //run 5 rounds
-  for (i = 0; i < 5; i++) {
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
+  cleanupResultsChildren();
+  const resultSection = document.querySelector(".result");
 
-    console.log(`Round ${round}: \n Player: ${humanChoice} vs. Computer: ${computerChoice}`); //print out choices in console
+  //display round #
+  const roundText = document.createElement("p");
+  roundText.textContent = `Round ${round}:`;
+  resultSection.appendChild(roundText);
 
-    //determine winner
-    if (humanChoice == computerChoice) {
-      console.log(`Round ${round} tied!`);
-    } else if (
-      (humanChoice == "rock" && computerChoice == "scissors") ||
-      (humanChoice == "paper" && computerChoice == "rock") ||
-      (humanChoice == "scissors" && computerChoice == "paper")
-    ) {
-      playerTally++;
-      console.log(`Round ${round} won!`);
-    } else {
-      computerTally++;
-      console.log(`Round ${round} lost!`);
-    }
-    round++;
+  //display player & computer choices
+  const choicesText = document.createElement("p");
+  choicesText.textContent = `Player: ${humanChoice} vs. Computer: ${computerChoice}`;
+  resultSection.appendChild(choicesText);
+
+  const winText = document.createElement("p");
+
+  //determine winner
+  if (humanChoice == computerChoice) {
+    winText.textContent = `Round ${round} tied!`;
+    resultSection.appendChild(winText);
+  } else if (
+    (humanChoice == "rock" && computerChoice == "scissors") ||
+    (humanChoice == "paper" && computerChoice == "rock") ||
+    (humanChoice == "scissors" && computerChoice == "paper")
+  ) {
+    playerTally++;
+    winText.textContent = `Round ${round} won!`;
+    resultSection.appendChild(winText);
+  } else {
+    computerTally++;
+    winText.textContent = `Round ${round} lost!`;
+    resultSection.appendChild(winText);
   }
 
-  console.log(`\nFinal score: \nPlayer: ${playerTally} \nComputer: ${computerTally}`); //print out final tally in console
+  //display tally
+  const scoreText = document.createElement("p");
+
+  if (playerTally < 5 && computerTally < 5) {
+    scoreText.textContent = `Current score: Player: ${playerTally} Computer: ${computerTally}`;
+    resultSection.appendChild(scoreText);
+  } else {
+    if (playerTally > computerTally) {
+      scoreText.innerHTML = `Final score: Player: ${playerTally} Computer: ${computerTally}. Player wins!`;
+      resultSection.appendChild(scoreText);
+    } else {
+      scoreText.innerHTML = `Final score: Player: ${playerTally} Computer: ${computerTally}. Computer wins!`;
+      resultSection.appendChild(scoreText);
+    }
+
+    const againText = document.createElement("p");
+    againText.textContent = "Press Reset to play again.";
+    resultSection.appendChild(againText);
+
+    const buttons = document.querySelectorAll("button.choice");
+    buttons.forEach(function (button) {
+      button.disabled = true; // disable each button
+    });
+  }
 }
 
-playRound();
+const btnChoice = document.querySelectorAll("button.choice");
+btnChoice.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    playRound(e.target.id);
+  });
+});
+
+function cleanupResultsChildren() {
+  const resultSection = document.querySelector(".result");
+
+  const children = resultSection.children; //get all children nodes from resultSection
+  while (children.length > 0) {
+    resultSection.removeChild(children[0]); //remove the first child until none are left
+  }
+}
+
+const btnReset = document.querySelector("#reset");
+btnReset.addEventListener("click", () => {
+  round = 0;
+  playerTally = 0;
+  computerTally = 0;
+
+  cleanupResultsChildren();
+
+  const buttons = document.querySelectorAll("button.choice");
+  buttons.forEach(function (button) {
+    button.disabled = false; // enable each button
+  });
+});
